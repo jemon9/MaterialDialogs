be7424c6c862c265e493764d48d2adcd86524415
unknown <>
2017-06-26 21:15:39 +0800

    6/26

--- core/src/main/java/njtech/nanjing/com/core/MaterialDialog.java
+++ core/src/main/java/njtech/nanjing/com/core/MaterialDialog.java
@@ -2,11 +2,23 @@ package njtech.nanjing.com.core;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.annotation.StyleRes;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.LayoutInflater;
 
 import njtech.nanjing.com.core.internal.MDRootLayout;
+import njtech.nanjing.com.core.utils.DialogUtils;
 
 /**
  * Created by 张志付 on 2017/6/24.
@@ -20,7 +32,7 @@ public class MaterialDialog extends DialogBase {
         super(builder.context, DialogInit.getTheme(builder));
         this.builder = builder;
         LayoutInflater inflater = LayoutInflater.from(builder.context);
-        view = (MDRootLayout) inflater.inflate(DialogInit.getInflateLayout(builder),null);
+        view = (MDRootLayout) inflater.inflate(DialogInit.getInflateLayout(builder), null);
         DialogInit.init(this);
     }
 
@@ -29,10 +41,114 @@ public class MaterialDialog extends DialogBase {
      */
     public static class Builder {
         protected final Context context;
+        protected int widgetColor;
+        protected ColorStateList positiveColor;
+        protected ColorStateList negativeColor;
+        protected ColorStateList neutralColor;
+        protected int buttonRippleColor = 0;
+        protected int titleColor = -1;
+        protected int contentColor = -1;
+        protected boolean titleColorSet = false;
+        protected Drawable icon;
+        protected GravityEnum titleGravity = GravityEnum.START;
+        protected GravityEnum contentGravity = GravityEnum.START;
+        protected GravityEnum btnStackedGravity = GravityEnum.END;
+        protected GravityEnum itemsGravity = GravityEnum.START;
+        protected GravityEnum buttonsGravity = GravityEnum.START;
+        protected CharSequence title;
 
 
         public Builder(Context context) {
             this.context = context;
+            final int materialBlue = DialogUtils.getColor(context, R.color.md_material_blue_600);
+            this.widgetColor = DialogUtils.resolveColor(context, R.attr.colorAccent, materialBlue);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                this.widgetColor = DialogUtils.resolveColor(context, android.R.attr.colorAccent,
+                        this.widgetColor);
+            }
+            positiveColor = DialogUtils.getActionTextStateList(context, this.widgetColor);
+            negativeColor = DialogUtils.getActionTextStateList(context, this.widgetColor);
+            neutralColor = DialogUtils.getActionTextStateList(context, this.widgetColor);
+            int fallback = 0;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                fallback = DialogUtils.resolveColor(context, android.R.attr.colorControlHighlight);
+            }
+            this.buttonRippleColor = DialogUtils.resolveColor(context, R.attr
+                    .md_btn_ripple_color, DialogUtils.resolveColor(context, R.attr
+                    .colorControlHighlight, fallback));
+
+            this.titleGravity = DialogUtils.resolveGravityEnum(context, R.attr.md_title_gravity,
+                    this.titleGravity);
+            this.contentGravity = DialogUtils.resolveGravityEnum(context, R.attr
+                    .md_content_gravity, this.contentGravity);
+            this.btnStackedGravity = DialogUtils.resolveGravityEnum(context, R.attr
+                    .md_btnstacked_gravity, this.btnStackedGravity);
+            this.itemsGravity = DialogUtils.resolveGravityEnum(context, R.attr.md_items_gravity,
+                    this.itemsGravity);
+            this.buttonsGravity = DialogUtils.resolveGravityEnum(context, R.attr
+                    .md_buttons_gravity, this.buttonsGravity);
+        }
+
+        public final Context getContext() {
+            return context;
+        }
+
+        public Builder title(@StringRes int titleRes) {
+            this.title(this.context.getText(titleRes));
+            return this;
+        }
+
+        public Builder title(@NonNull CharSequence text) {
+            this.title = text;
+            return this;
+        }
+
+        public Builder titleGravity(@NonNull GravityEnum gravityEnum) {
+            this.titleGravity = gravityEnum;
+            return this;
+        }
+
+        public Builder buttonRippleColor(@ColorInt int color) {
+            this.buttonRippleColor = color;
+            return this;
+        }
+
+        public Builder buttonRippleColorRes(@ColorRes int colorRes) {
+            return buttonRippleColor(DialogUtils.getColor(context, colorRes));
+
+        }
+
+        public Builder buttonRippleColorAttr(@AttrRes int colorAttr) {
+            return buttonRippleColor(DialogUtils.resolveColor(context, colorAttr));
+        }
+
+        public Builder titleColor(@ColorInt int titleColor) {
+            this.titleColor = titleColor;
+            this.titleColorSet = true;
+            return this;
+        }
+
+        public Builder titleColorRes(@ColorRes int titleColorRes) {
+            return titleColor(DialogUtils.getColor(context, titleColorRes));
+        }
+
+        public Builder titleColorAttr(@AttrRes int titleColorAttr) {
+            return titleColor(DialogUtils.resolveColor(context, titleColorAttr));
+        }
+
+        public Builder icon(@NonNull Drawable icon){
+            this.icon = icon;
+            return this;
+        }
+
+        public Builder iconRes(@DrawableRes int icon){
+            this.icon = ResourcesCompat.getDrawable(getContext().getResources(),icon,null);
+            return this;
+        }
+
+        public Builder iconAttr(@AttrRes int iconAttr){
+            this.icon = DialogUtils.resolveDrawable(context,iconAttr);
+            return this;
         }
     }
 }
--- core/src/main/java/njtech/nanjing/com/core/utils/DialogUtils.java
+++ core/src/main/java/njtech/nanjing/com/core/utils/DialogUtils.java
@@ -1,9 +1,15 @@
 package njtech.nanjing.com.core.utils;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.AttrRes;
 import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
+
+import njtech.nanjing.com.core.GravityEnum;
 
 /**
  * Created by 张志付 on 2017/6/25.
@@ -18,19 +24,83 @@ public class DialogUtils {
 
     /**
      * 根据attr的值获取颜色的int值
+     *
      * @param context
      * @param attr
      * @param defValue
      * @return
      */
     @ColorInt
-    private static int resolveColor(Context context, @AttrRes int attr, int defValue) {
+    public static int resolveColor(Context context, @AttrRes int attr, int defValue) {
         TypedArray a = context.getTheme().obtainStyledAttributes(new int[]{attr});
         try {
-            return a.getColor(0,defValue);
+            return a.getColor(0, defValue);
         } finally {
             a.recycle();
         }
 
+    }
+
+    public static int getColor(Context context, @ColorRes int colorId) {
+        return ContextCompat.getColor(context, colorId);
+    }
+
+    public static ColorStateList getActionTextStateList(Context context, int newPrimaryColor) {
+
+        return null;
+    }
+
+    /**
+     * get GravityEnum value from attr
+     *
+     * @param context
+     * @param attr
+     * @param defValue
+     * @return
+     */
+    public static GravityEnum resolveGravityEnum(Context context, @AttrRes int attr, GravityEnum
+            defValue) {
+        TypedArray a = context.obtainStyledAttributes(new int[]{attr});
+        try {
+            switch (a.getInt(0, gravityEnumToAttrInt(defValue))) {
+                case 1:
+                    return GravityEnum.CENTER;
+                case 2:
+                    return GravityEnum.END;
+                default:
+                    return GravityEnum.START;
+            }
+        } finally {
+            a.recycle();
+        }
+    }
+
+    private static int gravityEnumToAttrInt(GravityEnum defValue) {
+        switch (defValue) {
+            case CENTER:
+                return 1;
+            case END:
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    public static Drawable resolveDrawable(Context context, @AttrRes int iconAttr) {
+        return resolveDrawable(context, iconAttr, null);
+    }
+
+    private static Drawable resolveDrawable(Context context, @AttrRes int iconAttr, Drawable
+            fallback) {
+        TypedArray a = context.obtainStyledAttributes(new int[]{iconAttr});
+        try {
+            Drawable d = a.getDrawable(0);
+            if (d == null && fallback != null) {
+                d = fallback;
+            }
+            return d;
+        } finally {
+            a.recycle();
+        }
     }
 }
